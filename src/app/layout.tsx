8815e5add76a47fb6672495d263d440ac4968697
Tiaan Wolmarans <t@imtiaan.com>
2026-01-22 10:00:35 +0000

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${blockScript.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
